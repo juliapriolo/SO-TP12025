a2e418126eacfc8525969f09e28329e44f0a9f0e
Sebastian Vega Garrot <svegagarrot@itba.edu.ar>
2025-08-21 09:46:42 -0300

    Borre todo

--- test/clean_shm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-for name in /game_state /game_sync; do
-  if [ -e "/dev/shm${name}" ]; then
-    echo "Removing /dev/shm${name}"
-    rm -f "/dev/shm${name}"
-  fi
-done
